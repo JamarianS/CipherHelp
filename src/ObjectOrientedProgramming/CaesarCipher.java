f05c944cd9cbf06a3a7eed4e1db71292deee58ef
Jamarian St.Aimee <jstaimee@ggc.edu>
2023-04-22 19:15:39 -0400

    f

--- src/ObjectOrientedProgramming/CaesarCipher.java
+++ src/ObjectOrientedProgramming/CaesarCipher.java
@@ -1,2 +1,60 @@
-package ObjectOrientedProgramming;public class CaesarCipher {
-}
+package ObjectOrientedProgramming;
+
+import java.util.Scanner;
+
+public class CaesarCipher {
+
+    private int shift;
+
+    //no-arg constructor
+    public CaesarCipher(){
+        this(3); //default shift value
+    }
+
+    //constructor taking a parameter
+    public CaesarCipher(int shift){
+        this.shift = shift;
+    }
+
+
+    //here is example of encrypt method.
+    public String encrypt(String message) {
+        String encrypted = "";
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                char shifted = (char) ('A' + (c - 'A' + shift) % 26);
+                encrypted += shifted;
+            } else if (c >= 'a' && c <= 'z') {
+                char shifted = (char) ('a' + (c - 'a' + shift) % 26);
+                encrypted += shifted;
+            } else {
+                encrypted += c;
+            }
+        }
+        return encrypted;
+    }
+
+
+    //can you complete decrypt() method
+    public String decrypt(String message){
+        String decrypted = "";
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                char shifted = (char) ('A' + (c - 'A' - shift) % 26);
+                decrypted += shifted;
+            } else if (c >= 'a' && c <= 'z') {
+                char shifted = (char) ('a' + (c - 'a' - shift) % 26);
+                decrypted += shifted;
+            } else {
+                decrypted += c;
+            }
+        }
+        return decrypted;
+    }
+    }
+
+
+
+
--- src/ObjectOrientedProgramming/Main.java
+++ src/ObjectOrientedProgramming/Main.java
@@ -1,5 +1,26 @@
+package ObjectOrientedProgramming;
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String message = input.nextLine();
+        System.out.println("Enter 1 for this message to be Encrypted or 2 if you want it Decrypted: ");
+        int choice = input.nextInt();
+
+        CaesarCipher cc = new CaesarCipher();
+
+        if(choice == 1){
+            System.out.println("Encrypted message is: " + cc.encrypt(message));
+        }
+        else if(choice == 2){
+            System.out.println(cc.decrypt(cc.encrypt(message)));
+        }
+        else{
+            System.out.println("Invalid Input, Please Try Again!");
+        }
     }
 }
